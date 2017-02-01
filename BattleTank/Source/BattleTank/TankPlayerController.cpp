108443ca8d522f6724b9e2f271f5d018ab500037
PanayiotisPh <pr.filippou@gmail.com>
2017-02-01 15:46:57 +0200

    11 Find Screen Pixel Coordinates

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -44,7 +44,7 @@ void ATankPlayerController::AimTowardsCrosshair()
 	FVector HitLocation; // Out parameter
 	if (GetSightRayHitLocation(HitLocation)) // Is going to line trace
 
-	UE_LOG(LogTemp, Warning, TEXT("Hit Location: %s"), *HitLocation.ToString());
+	UE_LOG(LogTemp, Warning, TEXT("Look Direction: %s"), *HitLocation.ToString());
 
 	// If it hits landscape
 		// TODO Tell controlled tank to aim at this point	
@@ -53,6 +53,13 @@ void ATankPlayerController::AimTowardsCrosshair()
 // Get world location from lintrace through crosshair, true if it hits somewhere
 bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const
 {
-	HitLocation = FVector(1.0);
+	// Find the crosshair possition
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	auto ScreenLocation = FVector2D(ViewportSizeX * CrosshairXLocation, ViewportSizeY * CrosshairYLocation);
+
+	UE_LOG(LogTemp, Warning, TEXT("ScreenLocation: %s"), *ScreenLocation.ToString());
+	// De-project the screen position of the crosshair to a world direction
+	// Line-trace along that look direction, and see what we hit (upt to max range)
 	return true;
 }
--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -14,8 +14,8 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
-public:
-	ATank* GetControllerTank() const;
+private:
+	 ATank* GetControllerTank() const;
 
 	 virtual void BeginPlay() override;
 
@@ -27,4 +27,11 @@ public:
 	 // Return an OUT parameter, true if hit landscape
 	 bool GetSightRayHitLocation(FVector& HitLocation) const;
 	
+	 UPROPERTY(EditAnywhere)
+		 float CrosshairXLocation = 0.5;
+
+	 UPROPERTY(EditAnywhere)
+		 float CrosshairYLocation = 0.3333;
+
+
 };
